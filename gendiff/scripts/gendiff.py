2c4da1a9d61afdf95b6112fb7f5aa11c90f14c82
Pavel Goshurenko <goshurenkopl@gmail.com>
2020-03-12 18:55:28 +0200

    add argparse

---
+++ gendiff/scripts/gendiff.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+
+import argparse
+
+def main():
+    parser = argparse.ArgumentParser(description='Generate diff')
+    parser.add_argument('first_file')
+    parser.add_argument('second_file')
+    args = parser.parse_args()
+    print(args.accumulate(args.integers))
+    print("Welcome to to")
+    
+
+if __name__ == '__main__':
+    main()
