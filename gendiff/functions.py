9b5a276d1eaae1bf86a2ae4df5db053fd923377b
Pavel Goshurenko <goshurenkopl@gmail.com>
2020-03-14 12:14:36 +0200

    add generate_diff

---
+++ gendiff/functions.py
@@ -0,0 +1,39 @@
+import json
+
+def dictionary2str(dictionary, mark):
+    string = ''
+    for index, value in dictionary.items():
+        if len(mark) == 2:
+            string += "  {} {}: {}\n  {} {}: {}\n".format(mark[0], index, value[0], mark[1], index, value[1])
+        else:
+            string += "  {} {}: {}\n".format(mark, index, value)
+    return string
+
+
+
+def generate_diff(path_to_file1, path_to_file2):
+    before = json.load(open(path_to_file1))
+    after = json.load(open(path_to_file2))
+    same = {}
+    plus = {}
+    minus = {}
+    change = {}
+    for index, value in before.items():
+        if after.get(index, None) is None:
+            minus[index] = value
+            continue
+        if after.get(index, None) == value:
+            same[index] = value
+        else:
+            change[index] = (after[index], before[index])
+    for index, value in after.items():
+        if before.get(index, None) is None:
+            plus[index] = value
+    string = "{}{}{}{}".format(
+        dictionary2str(same, ' '),
+        dictionary2str(change, '+-'),
+        dictionary2str(minus, '-'),
+        dictionary2str(plus, '+')
+        )
+    string = "{\n" + string + "}"
+    return string
--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -2,14 +2,15 @@
 
 
 import argparse
+from gendiff.functions import generate_diff
 
 def main():
     parser = argparse.ArgumentParser(description='Generate diff')
-    parser.add_argument('first_file')
-    parser.add_argument('second_file')
+    parser.add_argument('first_file', type=str)
+    parser.add_argument('second_file', type=str)
+    parser.add_argument('-f', '--format' ,help='set format of output')
     args = parser.parse_args()
-    print(args.accumulate(args.integers))
-    print("Welcome to to")
+    print(generate_diff(args.first_file, args.second_file))
     
 
 if __name__ == '__main__':
