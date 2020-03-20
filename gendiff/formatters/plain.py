9d9a02886fc14e353d2ab2df7d9fce9ac91af14a
Pavel Goshurenko <goshurenkopl@gmail.com>
2020-03-20 11:49:47 +0200

    small corrections

--- gendiff/formatters/plain.py
+++ gendiff/formatters/plain.py
@@ -13,21 +13,12 @@ def plain_render(diff):
         for index, value in diff['complex'].items():
             string += plain_render(add_parent_name(value, index))
     for index, value in diff['change'].items():
-        if isinstance(value[0], dict):
-            text1 = 'complex value'
-        else:
-            text1 = value[0]
-        if isinstance(value[1], dict):
-            text2 = 'complex value'
-        else:
-            text2 = value[1]
+        text1 = 'complex value' if isinstance(value[0], dict) else value[0]
+        text2 = 'complex value' if isinstance(value[1], dict) else value[1]
         string += "Property '{}' was changed. From '{}' to '{}'\n".format(index, text1, text2) # noqa E501
     for index in diff['minus'].keys():
         string += "Property '{}' was removed\n".format(index)
     for index, value in diff['plus'].items():
-        if isinstance(value, dict):
-            text = 'complex value'
-        else:
-            text = value
+        text = 'complex value' if isinstance(value, dict) else value
         string += "Property '{}' was added with value: '{}'\n".format(index, text) # noqa E501
     return string
